882ab7d956a37ab7eb298fd9bd685181b1f58c07
Marcelo <santosmarcelo1995@live.com>
2020-06-23 13:22:40 -0300

    Nivel 1 - Typescript

---
+++ Nivel_1/typescript/src/routes.ts
@@ -0,0 +1,23 @@
+import {Request, Response} from 'express';
+import createUser from './services/CreateUser';
+
+export function helloWorld (request : Request, response : Response){
+    const user = createUser({
+        name : 'Marcelo', 
+        email : 'marcelo@email.com', 
+        password : '123456',
+        techs : [
+            'Node.js', 
+            'ReactJS', 
+            'React Native', 
+            {
+                title : 'Javascript',
+                experience : 100,
+            }
+        ],
+    });
+
+    console.log(user);
+
+    return response.json({message : 'Hello World'});
+}

6b782a9e4610641dbf81f7e53dcaef279627ef00
Marcelo <santosmarcelo1995@live.com>
2020-10-24 13:03:31 -0300

    Add S3 config

--- Nivel_4/iniciando-back-end/src/modules/users/infra/typeorm/entities/User.ts
+++ Nivel_4/iniciando-back-end/src/modules/users/infra/typeorm/entities/User.ts
@@ -9,6 +9,8 @@ import {
 
 import { Exclude, Expose } from 'class-transformer';
 
+import uploadConfig from '@config/upload';
+
 @Entity('users')
 class Users {
     @PrimaryGeneratedColumn('uuid')
@@ -35,9 +37,18 @@ class Users {
 
     @Expose({ name: 'avatar_url' })
     getAvatarUrl(): string | null {
-        return this.avatar
-            ? `${process.env.APP_API_URL}/files/${this.avatar}`
-            : null;
+        if (!this.avatar) {
+            return null;
+        }
+
+        switch (uploadConfig.driver) {
+            case 'disk':
+                return `${process.env.APP_API_URL}/files/${this.avatar}`;
+            case 's3':
+                return `${uploadConfig.config.aws.bucket}/files/${this.avatar}`;
+            default:
+                return null;
+        }
     }
 }
 

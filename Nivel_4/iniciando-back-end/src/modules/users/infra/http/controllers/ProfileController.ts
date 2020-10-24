2764fc0e0a2134ae3c5349ec0a3c1346d6ff75f4
Marcelo <santosmarcelo1995@live.com>
2020-10-24 10:17:49 -0300

    Add Class transformer

--- Nivel_4/iniciando-back-end/src/modules/users/infra/http/controllers/ProfileController.ts
+++ Nivel_4/iniciando-back-end/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ShowProfileService from '@modules/users/services/ShowProfileService';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
@@ -11,7 +12,7 @@ export default class ProfileController {
 
         const user = await showProfileService.execute({ user_id });
 
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 
     public async update(
@@ -31,6 +32,6 @@ export default class ProfileController {
             password,
         });
 
-        return response.json(user);
+        return response.json(classToClass(user));
     }
 }
--- Nivel_4/iniciando-back-end/src/modules/users/infra/http/controllers/UsersController.ts
+++ Nivel_4/iniciando-back-end/src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 
@@ -14,8 +15,6 @@ export default class UsersController {
 
         const user = await createUser.execute({ name, email, password });
 
-        delete user.password;
-
-        return response.status(200).json(user);
+        return response.status(200).json(classToClass(user));
     }
 }

0a3104b6bfc6a846fdbd2f0cc66cc7705a610b64
Marcelo <santosmarcelo1995@live.com>
2020-10-27 17:26:11 -0300

    Frontend Nivel 5 -  initial commit

---
+++ Nivel_5/go-barber-reactjs/src/pages/SignIn/index.tsx
@@ -0,0 +1,111 @@
+import React, { useRef, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+
+import * as Yup from 'yup';
+
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
+
+import getValidationErrors from '../../utils/getValidationErrors';
+
+import logo from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background, AnimationContainer } from './styles';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
+const SigIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  const { signIn } = useAuth();
+  const { addToast } = useToast();
+  const history = useHistory();
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('Email obrigatório!')
+            .email('Digite um email válido!'),
+          password: Yup.string().required('Senha obrigatória.'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+
+        await signIn({
+          email: data.email,
+          password: data.password,
+        });
+
+        history.push('/dashboard');
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description:
+            'Não foi possivel realizar login. Verifique suas credenciais.',
+        });
+      }
+    },
+    [signIn, addToast, history],
+  );
+
+  return (
+    <>
+      <Container>
+        <Content>
+          <AnimationContainer>
+            <img src={logo} alt="" />
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <h1>Faça seu logon</h1>
+
+              <Input name="email" icon={FiMail} placeholder="E-mail" />
+              <Input
+                name="password"
+                type="password"
+                icon={FiLock}
+                placeholder="Password"
+              />
+
+              <Button type="submit">Entrar</Button>
+
+              <a href="forgot">Esqueci a senha</a>
+            </Form>
+
+            <Link to="signup">
+              <FiLogIn />
+              Criar conta
+            </Link>
+          </AnimationContainer>
+        </Content>
+        <Background />
+      </Container>
+    </>
+  );
+};
+
+export default SigIn;

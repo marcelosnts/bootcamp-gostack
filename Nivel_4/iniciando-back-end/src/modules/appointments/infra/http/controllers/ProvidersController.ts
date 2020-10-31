8888930117d3584f85fd812417437fc0f2ee21f9
Marcelo <santosmarcelo1995@live.com>
2020-10-31 14:46:07 -0300

    Api adjusts

--- Nivel_4/iniciando-back-end/src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ Nivel_4/iniciando-back-end/src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
+import { classToClass } from 'class-transformer';
+
 import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
 export default class ProvidersController {
@@ -16,6 +18,6 @@ export default class ProvidersController {
             user_id,
         });
 
-        return response.json(providers);
+        return response.json(classToClass(providers));
     }
 }
